3ba3bebed895a6c6d21ed9509fa1bd5af356579a
Daniel Wagner-Hall <dawagner@gmail.com>
2024-12-19 23:19:52 +0000

    Add PR CI workflow

---
+++ download-buildifier.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -euo pipefail
+
+case "$(uname)" in
+  "Darwin")
+    suffix="darwin-amd64"
+    ;;
+  "Linux")
+    suffix="linux-amd64"
+    ;;
+  *)
+    echo >&2 "Unknown uname $(uname)"
+    exit 1
+    ;;
+esac
+
+curl --fail -L -o /tmp/buildifier.exe "https://github.com/bazelbuild/buildtools/releases/download/v7.3.1/buildifier-${suffix}"
+chmod 0755 /tmp/buildifier.exe
