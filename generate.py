8934a210302197cce7315b6f4946f4f5ba41e0b2
Andrew Katson <katsonandrew3.5@gmail.com>
2025-02-15 12:58:49 -0800

    Update generate.py
    Make the file executable on Windows

--- generate.py
+++ generate.py
@@ -15,10 +15,6 @@ import pathlib
 import platform
 from typing import Dict
 
-if platform.system() == "Windows":
-    import win32security
-    import ntsecuritycon as con
-
 openssl_version = "3.3.1"
 
 nix_platforms = [
@@ -466,22 +462,9 @@ def copy_from_here_to(local_path: str, dst: str, executable: bool = False):
         os.path.dirname(__file__), local_path)), dst)
     if executable:
         if platform.system == "Windows":
-            make_file_executable_on_windows(dst)
+            os.access(dst, os.R_OK | os.W_OK | os.X_OK)
         else:
             os.chmod(dst, 0o755)
-
-def make_file_executable_on_windows(path: str):
-
-    everyone, _, _ = win32security.LookupAccountName ("", "Everyone")
-
-    sd = win32security.GetFileSecurity(path, win32security.DACL_SECURITY_INFORMATION)
-    dacl = sd.GetSecurityDescriptorDacl()   # instead of dacl = win32security.ACL()
-
-    dacl.AddAccessAllowedAce(win32security.ACL_REVISION, con.FILE_ALL_ACCESS, everyone)
-
-    sd.SetSecurityDescriptorDacl(1, dacl, 0)   # may not be necessary
-    win32security.SetFileSecurity(path, win32security.DACL_SECURITY_INFORMATION, sd)
-
 
 def add_to_metadata(openssl_module_dir, tag):
     metadata_path = pathlib.Path(os.path.join(
