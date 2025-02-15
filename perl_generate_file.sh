da5b226e83f5a8bdb7202bc9b51fc9bf238f8a69
Andrew Katson <katsonandrew3.5@gmail.com>
2025-02-14 19:30:08 -0800

    Update perl_generate_file.sh
    Fix command brackets

--- perl_generate_file.sh
+++ perl_generate_file.sh
@@ -8,16 +8,16 @@ if [[ "$#" -ne 1 ]]; then
 fi
 
 commands="$1"
-echo "Running $commands"
+echo "Running ${commands}"
 # Iterate over each command separated by comma
 IFS=',' read -ra commands_arr <<< "$commands"
 for command in "${commands_arr[@]}"; do
-    $command
+    ${command}
 
     # Get the last element of the command, the out file
     # and check for existence.
-    IFS=' ' read -ra split_command_arr <<< "$command"
-    out_file="$command[-1]"
+    IFS=' ' read -ra split_command_arr <<< "${command}"
+    out_file="${command[-1]}"
     if test -f ${out_file}; then
       echo "${out_file} exists"
     else
