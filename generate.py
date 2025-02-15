35d7f718d738f557c92ae06202f154a52d7ad8a9
Andrew Katson <katsonandrew3.5@gmail.com>
2025-02-14 17:35:51 -0800

    Update generate.py
    Remove print

--- generate.py
+++ generate.py
@@ -449,7 +449,6 @@ OPENSSL_VERSION = "{openssl_version}"
 
 GEN_FILES = {json_dump}
 """ 
-    print(f"katsonandrew: {out}")
     path = pathlib.Path(os.path.join(overlay_dir, f"constants-{platform}.bzl"))
     with open(pathlib.Path(path), "w") as f:
         f.write(out)
