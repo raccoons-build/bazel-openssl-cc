2503495e1fe370986c086c395625045a7fe7eda5
Andrew Katson <katsonandrew3.5@gmail.com>
2025-03-01 10:26:18 -0800

    Update extract_srcs.pl
    On Windows use \ instead of /

--- extract_srcs.pl
+++ extract_srcs.pl
@@ -65,7 +65,14 @@ my %libcrypto_srcs = get_recursive_srcs_of_one("libcrypto");
 my %excludes = %libcrypto_srcs;
 my %libssl_srcs = get_recursive_srcs_of_one("libssl", (), %excludes);
 %excludes = (%excludes, %libssl_srcs);
-my %openssl_app_srcs = get_recursive_srcs_of_one("apps/openssl", (), %excludes);
+my %openssl_app_srcs;
+# On Windows we need to modify the path since the configdata file 
+# will be using different paths delimitters.
+if ("$^O" eq "MSWin32") {
+    %openssl_app_srcs = get_recursive_srcs_of_one("apps\\openssl", (), %excludes);
+} else {
+    %openssl_app_srcs = get_recursive_srcs_of_one("apps/openssl", (), %excludes);
+}
 
 my %libcrypto_defines = get_recursive_defines("libcrypto");
 my %libssl_defines = get_recursive_defines("libssl", ("libcrypto", 1));
