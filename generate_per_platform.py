7bda1be46e7bd9211f1f1fa9a7622111f85cd7c1
Andrew Katson <katsonandrew3.5@gmail.com>
2025-03-15 12:48:21 -0700

    Fix typo and grab all files

--- generate_per_platform.py
+++ generate_per_platform.py
@@ -46,7 +46,7 @@ def main(bcr_dir: str, openssl_tar_path: str, tag: str, operating_system: str):
             with open(pathlib.Path(os.path.join(openssl_dir, 'openssl_info.json')), 'w') as fp:
                 json.dump(openssl_info, fp)
             # Just grab every file
-            files_to_tar = list(sorted(pathlib.Path(openssl_dir).rglob(pattern=".*")))
+            files_to_tar = list(sorted(pathlib.Path(openssl_dir).rglob(pattern="*")))
             tar = "gtar" if sys.platform == "darwin" else "tar"
             extra_tar_options = get_extra_tar_options(operating_system)
             subprocess.check_call([tar] + extra_tar_options + ["-czf", openssl_tar_path] + files_to_tar,
