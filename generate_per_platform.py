3d51b0c7d0ee2f5cd3d0b26572fdd8d2ee3b7985
Andrew Katson <katsonandrew3.5@gmail.com>
2025-03-15 13:04:50 -0700

    Keep files that are less than 260 in length on Windows

--- generate_per_platform.py
+++ generate_per_platform.py
@@ -5,7 +5,7 @@ import sys
 import pathlib
 import json
 
-from common import download_openssl, openssl_version, get_platforms, get_start_configure_list, get_make_command, generated_files, get_extra_tar_options, copy_from_here_to
+from common import download_openssl, openssl_version, get_platforms, get_start_configure_list, get_make_command, generated_files, get_extra_tar_options, copy_from_here_to, get_simple_platform, WINDOWS
 
 def main(bcr_dir: str, openssl_tar_path: str, tag: str, operating_system: str):
     openssl_module_dir = pathlib.Path(
@@ -46,7 +46,16 @@ def main(bcr_dir: str, openssl_tar_path: str, tag: str, operating_system: str):
             with open(pathlib.Path(os.path.join(openssl_dir, 'openssl_info.json')), 'w') as fp:
                 json.dump(openssl_info, fp)
             # Just grab every file
-            files_to_tar = list(sorted(pathlib.Path(openssl_dir).rglob(pattern="*")))
+            all_files_to_tar = list(sorted(pathlib.Path(openssl_dir).rglob(pattern="*")))
+            files_to_tar = []
+            simple_platform = get_simple_platform(operating_system)
+            if simple_platform == WINDOWS:
+                for file in all_files_to_tar:
+                    if len(file) <= 260: 
+                        files_to_tar.append(file)
+            else: 
+                files_to_tar = all_files_to_tar
+
             tar = "gtar" if sys.platform == "darwin" else "tar"
             extra_tar_options = get_extra_tar_options(operating_system)
             subprocess.check_call([tar] + extra_tar_options + ["-czf", openssl_tar_path] + files_to_tar,
