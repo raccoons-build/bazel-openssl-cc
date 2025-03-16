597b98be9e91e2657198b47fc168a2680c2d9299
Andrew Katson <katsonandrew3.5@gmail.com>
2025-03-16 15:06:27 -0700

    Update generate_combine_platforms.py
    Figure out what files are in openssl unix and windows dirs

--- generate_combine_platforms.py
+++ generate_combine_platforms.py
@@ -179,6 +179,9 @@ def download_openssl_files(openssl_dir: str):
     openssl_unix_dir = pathlib.Path(os.path.join(openssl_dir, "unix"))
     final_dest_path = pathlib.Path(os.path.join(openssl_dir, "combined"))
 
+    subprocess.check_call(["ls", "-R", openssl_windows_dir])
+    subprocess.check_call(["ls", "-R", openssl_unix_dir])
+
     # First we move windows 
     shutil.copytree(openssl_windows_dir, final_dest_path)
     # Then we move unix
