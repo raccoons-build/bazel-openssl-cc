4eb7497aa95330910ef0f46035344e0dc5821f4c
Daniel Wagner-Hall <dawagner@gmail.com>
2025-07-20 21:42:25 +0100

    Rewrite sha256 test in C++
    This avoids needing python and skylib dependencies.

--- generate_combine_platforms.py
+++ generate_combine_platforms.py
@@ -169,9 +169,13 @@ def main(
             pathlib.Path(os.path.join(overlay_dir, "test_bazel_build", "BUILD.bazel")),
         )
         copy_from_here_to(
-            "sha256_test.py",
-            pathlib.Path(os.path.join(overlay_dir, "test_bazel_build", "sha256_test.py")),
-            executable=True,
+            "sha256_test.cc",
+            pathlib.Path(os.path.join(overlay_dir, "test_bazel_build", "sha256_test.cc")),
+        )
+
+        copy_from_here_to(
+            "build_test.cc",
+            pathlib.Path(os.path.join(overlay_dir, "test_bazel_build", "build_test.cc")),
         )
 
         with open(pathlib.Path(os.path.join(output_tar_dir, "BUILD.bazel")), "w") as f:
@@ -225,10 +229,6 @@ def write_module_files(
 bazel_dep(name = "platforms", version = "0.0.11")
 bazel_dep(name = "rules_cc", version = "0.1.1")
 bazel_dep(name = "rules_perl", version = "0.4.1")
-
-# Just for testing.
-bazel_dep(name = "bazel_skylib", version = "1.7.1")
-bazel_dep(name = "rules_python", version = "1.2.0")
 
 http_archive = use_repo_rule("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
 
